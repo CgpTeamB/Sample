7531e0b6d2765ae75098af2931a725b28b1b4f3e
situtyo <situtyo666@gmail.com>
2015-12-11 19:46:34 +0900

    NewBehaviour
    new

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        int ClearStage = PlayerPrefs.GetInt("ClearStage");
+        Debug.Log(ClearStage);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
